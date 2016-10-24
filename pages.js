a4051af450b94dc7c4b1886f165f625edf0c64d5
Maya Mohsin Ahmed <maya.ahmed@gmail.com>
2016-10-24 15:09:32 +0530

    Add files via upload

--- pages.js
+++ pages.js
@@ -1,53 +1,65 @@
+function init(){
+  if(localStorage.pagesArray)
+    pagesArray = JSON.parse(localStorage.savedPages);
+  else  createPages();
+}
+
+
 container = document.getElementById('container');  
 pagesArray = new Array();
-var n = 10;
+ n = 11;
 
-/*
- pagesArray[0] = document.createElement("canvas");
- pagesArray[0].className= "page coverPage";
+pagesArray[0] = document.createElement("canvas");
  container.appendChild(pagesArray[0]);
+pagesArray[0].style.visibility="visible";
+pagesArray[0].className= "page coverPage";
 pagesArray[0].id= "page0";
-*/
 
-  for(var i=0; i<n; i++){
+function createPages(){
+ for(var i=1; i<n; i++){
   pagesArray[i] = document.createElement("canvas");
   container.appendChild(pagesArray[i]);
   var string = "page"+i;
   pagesArray[i].id= string;
+  if(i>0){
      pagesArray[i].className= "page";
     pagesArray[i].style.visibility="hidden";
-   
   }
-pagesArray[0].style.visibility="visible";
-  pagesArray[0].className= "page coverPage";
+ }
+}
+
+localStorage.savedPages = JSON.stringify(pagesArray);
 
 
 
-function nextPage(k){
 
-   var string1= "page"+k; 
-  var t=k+1;
-  var string2= "page"+t;
-var s1= document.getElementById(string1);
-var s2 = document.getElementById(string2);  
-s1.innerHTML=string1;
-s1.innerHTML=string2;
+
+function reachPage(k){
+  if(k > n-1) {alert("no no no"+n); k=0;}
   
-  if(k<4)  slideOpenPage(s1, s2);
-  else if(k==4) 
-location.reload(); 
-  }
-
-
-
-
-function slideOpenPage(el,newPage){
-console.log(el);
- 
-el.style.transition="width 4s linear 0s";
-el.style.width="0.5%";
- newPage.style.visibility="visible";
+var string= "page"+k; 
+var s= document.getElementById(string);
+hidePages(k);
+openPage(s);
  
 }
+
+function hidePages(j){
+  for(var i=0; i<n; i++){
+    if(i != j){
+      
+  pagesArray[i].style.visibility="hidden";}
+  }
+}
+
+
+
+function openPage(el){
+
+el.style.transition="width 4s linear 0s";
+el.style.width="350px";
+el.style.visibility="visible";
+}
+
 
 
